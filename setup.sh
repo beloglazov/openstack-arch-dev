ae324d628f039b5692a5d6663fd0d91d47bd5448
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-01 00:21:57 +1000

    Removed creating a python2 wrapper

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@
 sudo pacman -Syy
 sudo pacman -S swig python2-pip libvirt
 
-# replace python with python2
-echo "#!/bin/sh
-python2 $*" > ~/bin/python
-chmod +x ~/bin/python
-
 sudo pip2 install nose
 sudo pip2 install virtualenv
 
